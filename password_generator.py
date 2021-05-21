bc99d1e590322a16558a4c396f03b42a1c8ed84f
Jared Merchant <jaredmerchant@Jareds-MBP.attlocal.net>
2021-05-21 15:10:40 -0500

    Fixed validation issues

--- password_generator.py
+++ password_generator.py
@@ -13,7 +13,6 @@ config_rules = {
     'max_length': config['password_options']['max_length'],
     'default_length': config['password_options']['default_length']
 }
-print(config_rules)
 
 
 class bcolors:
@@ -25,6 +24,8 @@ class bcolors:
 # assign max_length from config
 print(f'{bcolors.WARNING}\nThe maximum password length is ' + config_rules['max_length'] + ' characters.')
 print(f'This can be altered in the config file.\n{bcolors.ENDC}')
+
+# prompts for desired password complexity
 options = [
     inquirer.Text('password_length',
                   message='Password Length'),
@@ -51,23 +52,28 @@ for i in character_categories:
         selected_characters = selected_characters + string.punctuation
 
 
+# validation rules
 def validation(rules):
     if password_length == '':
-        print(f'{bcolors.FAIL}No password length was defined.')
-        exit()
+        print(f'{bcolors.FAIL}No password length was defined.{bcolors.ENDC}')
+        print(password_length)
+        return
     elif selected_characters == '':
-        print(f'{bcolors.FAIL}No character categories were defined.')
-        exit()
-    elif password_length > rules['max_length']:
-        print('A password length of ' + password_length + ' is not allowed.')
-        print('The maximum password length is ' + rules['max_length'] + ' characters.')
-        exit()
+        print(selected_characters)
+        print(f'{bcolors.FAIL}No character categories were defined.{bcolors.ENDC}')
+        return
+    elif int(password_length) > int(rules['max_length']):
+        print(f'{bcolors.FAIL}A password length of ' + password_length + ' is not allowed.')
+        print('The maximum password length is ' + rules['max_length'] + f' characters.{bcolors.ENDC}')
+        return
     else:
+        # create and print password
         print('Generated Password:')
+        password = ''.join(secrets.choice(selected_characters) for i in range(int(password_length)))
+        print(password)
 
 
+# run validation function
 validation(config_rules)
 
-password = ''.join(secrets.choice(selected_characters) for i in range(int(password_length)))
-
-print(password)
+print('\nBuilt using the Python Secrets module.\n')
